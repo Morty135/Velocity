d069b72f4440a88c365abe9cbb4f72d2f8f89a7f
Morty135 <morty135135135@gmail.com>
2024-10-06 21:06:42 +0200

    innit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <raylib.h>
+
+
+int main()
+{
+    InitWindow(800,600, "Skyrunner");
+
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        
+        EndDrawing();        
+    }
+
+    CloseWindow();
+    
+}
